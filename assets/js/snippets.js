c1085ff4a91e51e0b3c8eb9638e28e1ae0f627f8
Ben Baumann <ben@thisisbam.com>
2017-09-18 20:52:36 -0400

    push it up baby

---
+++ assets/js/snippets.js
@@ -0,0 +1,21 @@
+var sidepanel = {
+  directive: "<social></social>",
+  template: `<div id="sidebar" class="card float-right">
+<h3>Connect With Me</h3>
+<ul>
+    <li class="float-left margin-right-20"><a href="#"><img src='https://dummyimage.com/63x63/CCC/000.gif?text=github' alt='github'/></a></li>
+    <li class="float-left margin-right-20"><a href="#"><img src='https://dummyimage.com/63x63/CCC/000.gif?text=linkedIn' alt='linked in'/></a></li>
+    <li class="float-left"><a href="#"><img src='https://dummyimage.com/63x63/CCC/000.gif?text=stack' alt='stack overflow'/></a></li>
+</ul>
+</div> <!-- end sidebar -->`
+};
+
+// var social = document.getElementsByTagName(social);
+
+document.addEventListener("DOMContentLoaded", function() {
+    var content = document.getElementById(content);
+    // var wrapper = document.getElementById(wrapper);
+    // var newNode = sidepanel.template;
+    // var newNode = "<h1>Banana</h1>";
+    // wrapper.insertBefore(newNode, content);
+  });
